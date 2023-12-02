459626c4b69127b6b791388b1396b1fd1b09803a
cloveds <147108292+cloveds@users.noreply.github.com>
2023-12-01 18:11:35 -0600

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,28 +9,28 @@ Plug('neoclide/coc.nvim', {['branch'] = 'release'})
 Plug 'tribela/vim-transparent'
 Plug 'https://github.com/rafi/awesome-vim-colorschemes'
 vim.call('plug#end')
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.signcolumn = "yes"
+vim.o.updatetime = 300
+vim.o.number = true
+vim.o.autoindent = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.smarttab = true
+vim.o.autoindent = true	
+vim.o.guifont = '*'
+vim.o.softtabstop = 4
+vim.o.encoding = 'utf8'
+vim.o.relativenumber= true
+vim.o.background = 'dark'
+vim.o.termguicolors = true
 vim.cmd[[
-set nobackup
-set nowritebackup
-set signcolumn=yes
-set updatetime=300
-set number
-set autoindent
-set tabstop=4
-set shiftwidth=4
-set smarttab
-set autoindent	
-set guifont=*
-set softtabstop=4
-set encoding=utf8
-set relativenumber
-set background=dark
-set termguicolors
-colorscheme one
-set guicursor=i:block
-set cursorlineopt=number
-hi LineNr ctermfg=NONE guibg=NONE guifg=#D3D3D3
-let g:pear_tree_smart_openers = 1
-let g:pear_tree_smart_closers = 1
-let g:pear_tree_smart_backspace = 1
+	colorscheme one
+	set guicursor=i:block
+	set cursorlineopt=number
+	hi LineNr ctermfg=NONE guibg=NONE guifg=#D3D3D3
 ]]
+vim.g.pear_tree_smart_openers = 1
+vim.g.pear_tree_smart_closers = 1
+vim.g.pear_tree_smart_backspace = 1
