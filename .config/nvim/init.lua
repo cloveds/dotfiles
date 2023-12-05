4f146c07410cc8dee2f3361a7311871e76bd58de
cloveds <147108292+cloveds@users.noreply.github.com>
2023-12-04 18:16:20 -0600

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,6 +8,9 @@ Plug 'https://github.com/rust-lang/rust.vim'
 Plug('neoclide/coc.nvim', {['branch'] = 'release'})
 Plug 'sheerun/vim-polyglot'
 Plug 'tribela/vim-transparent'
+Plug 'nvim-lualine/lualine.nvim'
+Plug 'christianchiarulli/nvcode-color-schemes.vim'
+Plug 'nvim-tree/nvim-web-devicons'
 Plug 'https://github.com/rafi/awesome-vim-colorschemes'
 vim.call('plug#end')
 vim.o.backup = false
@@ -26,18 +29,27 @@ vim.o.encoding = 'utf8'
 vim.o.relativenumber= true
 vim.o.background = 'dark'
 vim.cmd[[
+lua << END
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+	theme = 'onedark',
+	},
+}
+END
 syntax enable
+colorscheme one
 filetype plugin indent on
 function! CheckBackspace() abort
   let col = col('.') - 1
   return !col || getline('.')[col - 1]  =~# '\s'
-endfunction	
+endfunction
+
+set noshowmode
 set nocompatible
 set guifont=*
-colorscheme one 
 set guicursor=i:block
 set cursorlineopt=number
-set laststatus=0 ruler
 hi LineNr ctermfg=NONE guibg=NONE guifg=#D3D3D3
 inoremap <silent><expr> <TAB>
     \ coc#pum#visible() ? coc#pum#next(1) :
