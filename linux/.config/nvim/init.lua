aa472380906f17fd486750a336732b31197c0b50
cloveds <147108292+cloveds@users.noreply.github.com>
2023-12-07 07:02:16 -0600

    Update init.lua

--- linux/.config/nvim/init.lua
+++ linux/.config/nvim/init.lua
@@ -51,6 +51,4 @@ nnoremap <leader>n :NERDTreeFocus<CR>
 nnoremap <C-n> :NERDTree<CR>
 nnoremap <C-t> :NERDTreeToggle<CR>
 nnoremap <C-f> :NERDTreeFind<CR>
-autocmd VimEnter * NERDTree | wincmd p
-let g:NERDTreeWinPos = "left"
 ]]
