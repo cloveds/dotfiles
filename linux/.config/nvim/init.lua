9c5b709a7ad3590cc2b6c6c17d85ddc1e4efb17b
cloveds <147108292+cloveds@users.noreply.github.com>
2023-12-09 14:14:07 -0600

    Update init.lua

--- linux/.config/nvim/init.lua
+++ linux/.config/nvim/init.lua
@@ -4,65 +4,85 @@ Plug 'numToStr/Comment.nvim'
 Plug('fatih/vim-go', {['do'] = 'GoUpdateBinaries' })
 Plug('darrikonn/vim-gofmt', {['do'] = 'GoUpdateBinaries' })
 Plug 'https://github.com/rust-lang/rust.vim'
-Plug 'nvim-lualine/lualine.nvim'
-Plug 'nvim-tree/nvim-tree.lua'
+Plug 'williamboman/mason.nvim'
+Plug 'williamboman/mason-lspconfig.nvim'
 Plug 'nvim-tree/nvim-web-devicons'
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = 'TSUpdate'})
-Plug('neoclide/coc.nvim', {['branch'] = 'release'})
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'nvim-lua/plenary.nvim'
+Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.5' })
+Plug 'L3MON4D3/LuaSnip'
+Plug('VonHeikemen/lsp-zero.nvim', {['branch'] = 'v3.x'})
 Plug "olimorris/onedarkpro.nvim"
 Plug 'windwp/nvim-autopairs'
 vim.call('plug#end')
-require("nvim-tree").setup({
-	view = {
-		side = "right"
-	}
+
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
+	  'rust_analyzer',
+	  'lua_ls',
+	  'gopls',
+	  'bashls',
+	  'pyright',
+	  'asm_lsp',
+  },
+  handlers = {
+    lsp_zero.default_setup,
+  },
 })
-require("nvim-autopairs").setup {}
-require('Comment').setup()
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'onedark',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+	  "c",
+	  "lua",
+	  "vim",
+	  "rust",
+	  "python",
+	  "bash",
+	  "go",
   },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename', 'filetype'},
-    lualine_x = {'fileformat'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
   },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
 }
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-h>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+	['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  })
+})
+
+require("nvim-autopairs").setup {}
+
+require('Comment').setup()
+
 vim.o.backup = false
 vim.o.writebackup = false
 vim.o.signcolumn = "yes"
-vim.o.updatetime = 300
+vim.o.updatetime = 50
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.laststatus = 0
@@ -76,34 +96,10 @@ vim.o.background = "dark"
 vim.o.termguicolors = true
 vim.o.encoding = "utf-8"
 vim.o.compatible = false
-vim.o.showmode = false
 vim.o.cursorline = true
 vim.o.showtabline = 0
 vim.o.cursorlineopt = "number"
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "vim", "rust", "python", "bash" },
-  sync_install = false,
-  highlight = {
-    enable = true,
-  },
-}
 vim.o.guicursor = "i:block"
 vim.cmd.syntax = "enable"
--- vim.cmd.highlight({"CocFloating", "guibg=#44475A"})
--- vim.cmd.highlight({"CocMenuSel", "guibg=#282A36"})
 vim.cmd.colorscheme("onedark")
-vim.cmd[[
-filetype plugin indent on
-function! CheckBackspace() abort
-  let col = col('.') - 1
-  return !col || getline('.')[col - 1]  =~# '\s'
-endfunction
-inoremap <silent><expr> <TAB>
-    \ coc#pum#visible() ? coc#pum#next(1) :
-    \ CheckBackspace() ? "\<Tab>" :
-    \ coc#refresh()
-inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
-nnoremap <leader>n :NvimTreeFocus<CR>
-nnoremap <C-n> :NvimTreeToggle<CR>
-]]
-
+vim.cmd[[filetype plugin indent on]]
