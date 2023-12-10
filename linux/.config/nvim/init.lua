d561a773b55048dbe40161a31d0b7bc09d7a4d46
cloveds <147108292+cloveds@users.noreply.github.com>
2023-12-09 20:19:07 -0600

    Update init.lua

--- linux/.config/nvim/init.lua
+++ linux/.config/nvim/init.lua
@@ -4,39 +4,82 @@ Plug 'numToStr/Comment.nvim'
 Plug('fatih/vim-go', {['do'] = 'GoUpdateBinaries' })
 Plug('darrikonn/vim-gofmt', {['do'] = 'GoUpdateBinaries' })
 Plug 'https://github.com/rust-lang/rust.vim'
-Plug 'williamboman/mason.nvim'
-Plug 'williamboman/mason-lspconfig.nvim'
 Plug 'nvim-tree/nvim-web-devicons'
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = 'TSUpdate'})
-Plug 'neovim/nvim-lspconfig'
-Plug 'hrsh7th/nvim-cmp'
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'nvim-lua/plenary.nvim'
-Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.5' })
-Plug 'L3MON4D3/LuaSnip'
-Plug('VonHeikemen/lsp-zero.nvim', {['branch'] = 'v3.x'})
 Plug "olimorris/onedarkpro.nvim"
 Plug 'windwp/nvim-autopairs'
+Plug('VonHeikemen/lsp-zero.nvim', {['branch'] = 'v1.x'})
+Plug 'neovim/nvim-lspconfig'
+Plug 'williamboman/mason.nvim'
+Plug 'williamboman/mason-lspconfig.nvim'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'saadparwaiz1/cmp_luasnip'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-nvim-lua'
+Plug 'L3MON4D3/LuaSnip'
+Plug 'rafamadriz/friendly-snippets'
 vim.call('plug#end')
+ 
+local lsp = require("lsp-zero")
+lsp.preset("recommended")
 
-local lsp_zero = require('lsp-zero')
+lsp.ensure_installed({
+  'lua_ls',
+  'rust_analyzer',
+  'bashls',
+  'pyright',
+  'gopls'
+})
 
-lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
+lsp.nvim_workspace()
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+-- cmp_mappings['<Tab>'] = nil
+-- cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {
-	  'rust_analyzer',
-	  'lua_ls',
-	  'gopls',
-	  'bashls',
-	  'pyright',
-  },
-  handlers = {
-    lsp_zero.default_setup,
-  },
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
 })
 
 require'nvim-treesitter.configs'.setup {
@@ -55,24 +98,6 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
   },
 }
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-h>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
-cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-	['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  })
-})
 
 require("nvim-autopairs").setup {}
 
