e3f98fb24c454d219d6bda85790525583c047ba1
cloveds <147108292+cloveds@users.noreply.github.com>
2023-12-11 16:56:24 -0600

    Create install.sh

---
+++ linux/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip 
+unzip ~/.JetBrainsMono.zip -d ~/.local/share/fonts
+mv ~/dotfiles/linux/.config ~/.config
+mv ~/dotfiles/linux/.xinitrc ~/.xinitrc
+mv ~/dotfiles/linux/autostart.sh ~/autostart.sh
+chmod 755 autostart.sh
+git clone https://github.com/neovim/neovim
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+echo 'alias vi="nvim" 
+alias vim="nvim"' >> .bashrc
