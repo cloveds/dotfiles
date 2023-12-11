0837348be0b2cdc8cdb52238d5ea1562d51677ee
cloveds <147108292+cloveds@users.noreply.github.com>
2023-12-11 17:02:30 -0600

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -10,5 +10,5 @@ cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-echo 'alias vi="nvim" 
-alias vim="nvim"' >> .bashrc
+echo 'alias vi="nvim"' >> ~/.bashrc
+echo 'alias vim="nvim"' >> ~/.bashrc
