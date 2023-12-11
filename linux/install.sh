94f0111d954e62cdde285bfce1a84b8cec0bdf81
cloveds <147108292+cloveds@users.noreply.github.com>
2023-12-11 17:06:03 -0600

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt install unzip npm nodejs curl ninja-build gettext cmake spice-vdagent xfce4-terminal xorg i3 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip 
 unzip ~/JetBrainsMono.zip -d ~/.local/share/fonts
 mv ~/dotfiles/linux/.config ~/.config
