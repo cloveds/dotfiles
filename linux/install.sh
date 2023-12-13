52a2240a6207d0d2ca9f7fd92587788d3a817e4c
cloveds <147108292+cloveds@users.noreply.github.com>
2023-12-13 15:02:21 -0600

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -16,8 +16,8 @@ git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 sleep 1
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 sleep 1
 mv ~/dotfiles/linux/.zshrc ~/.zshrc
 chsh -s $(which zsh)
