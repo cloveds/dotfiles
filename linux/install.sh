b4b006306b0ed1b2b12eeb2b3420943c9a5627de
cloveds <147108292+cloveds@users.noreply.github.com>
2023-12-13 08:22:31 -0600

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 sudo apt install unzip npm nodejs curl ninja-build gettext cmake spice-vdagent xfce4-terminal xorg i3 polybar firefox-esr zsh zsh-syntax-highlighting -y
+sleep 1
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
 mkdir .local .local/share
 unzip ~/JetBrainsMono.zip -d ~/.local/share/fonts
+sleep 1
 mv ~/dotfiles/linux/.config ~/.config
 mv ~/dotfiles/linux/.xinitrc ~/.xinitrc
 mv ~/dotfiles/linux/autostart.sh ~/autostart.sh
+sleep 1
 chmod +x ~/.config/polybar/launch.sh
 chmod 755 autostart.sh
+sleep 1
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
+sleep 1
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sleep 1
 mv ~/dotfiles/linux/.zshrc ~/.zshrc
 chsh -s $(which zsh)
