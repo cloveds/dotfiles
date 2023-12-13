c5541467c527f9224f8526f38fef8ea49a22fc2f
cloveds <147108292+cloveds@users.noreply.github.com>
2023-12-13 12:26:34 -0600

    Create treesitter.lua

---
+++ linux/.config/nvim/after/plugin/treesitter.lua
@@ -0,0 +1,16 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+	  "c",
+	  "lua",
+	  "vim",
+	  "rust",
+	  "python",
+	  "bash",
+	  "go",
+  },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+  },
+}
