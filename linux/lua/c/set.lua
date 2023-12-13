0f7e3becd14bbea1b9dd6ce0ca6008220c799e4e
cloveds <147108292+cloveds@users.noreply.github.com>
2023-12-13 11:42:45 -0600

    Create set.lua

---
+++ linux/lua/c/set.lua
@@ -0,0 +1,24 @@
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.signcolumn = "yes"
+vim.o.updatetime = 50
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.laststatus = 0
+vim.o.cmdheight = 1
+vim.o.autoindent = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.smarttab = true
+vim.o.softtabstop = 4
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.o.encoding = "utf-8"
+vim.o.compatible = false
+vim.o.cursorline = true
+vim.o.showtabline = 0
+vim.o.cursorlineopt = "number"
+vim.o.guicursor = "i:block"
+vim.cmd.syntax = "enable"
+vim.cmd.colorscheme("onedark")
+vim.cmd[[filetype plugin indent on]]
