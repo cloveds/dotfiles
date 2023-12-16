81de27797a95461f787ecf99f7cdf1e168c09636
cloveds <147108292+cloveds@users.noreply.github.com>
2023-12-16 17:54:56 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt install build-essential npm nodejs unzip curl ninja-build gettext cmake spice-vdagent xfce4-terminal xorg i3 firefox-esr libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev golang-go libffi-dev liblzma-dev -y
+sudo apt install build-essential npm nodejs unzip curl luarocks ninja-build gettext cmake spice-vdagent xfce4-terminal xorg i3 firefox-esr libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev golang-go libffi-dev liblzma-dev -y
 sleep 2
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
 mkdir .local .local/share
@@ -12,6 +12,8 @@ sleep 2
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
+sleep 2
+luarocks install --server=https://luarocks.org/dev luaformatter
 sleep 2
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 cd ~/.pyenv && src/configure && make -C src
