651734df9b1afbb198f1df0402fa7c53a55e5481
cloveds <147108292+cloveds@users.noreply.github.com>
2023-12-16 16:34:53 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
-sudo apt install unzip curl ninja-build gettext cmake spice-vdagent xfce4-terminal xorg i3 firefox-esr -y
-sleep 1
+sudo apt install unzip curl ninja-build gettext cmake spice-vdagent xfce4-terminal xorg i3 firefox-esr libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev-y
+sleep 2
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
-sleep 1
 mkdir .local .local/share
-sleep 1
 unzip ~/JetBrainsMono.zip -d ~/.local/share/fonts
-sleep 1
 mv ~/dotfiles/linux/.config ~/.config
-sleep 1
 mv ~/dotfiles/linux/.xinitrc ~/.xinitrc
-sleep 1
 mv ~/dotfiles/linux/autostart.sh ~/autostart.sh
-sleep 1
 chmod 755 autostart.sh
-sleep 1
+sleep 2
 git clone https://github.com/neovim/neovim
-sleep 1
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
-sleep 1
 sudo make install
+sleep 2
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+cd ~/.pyenv && src/configure && make -C src
 sleep 1
 echo alias vi="nvim" >> ~/.bashrc
-sleep 1
 echo alias vim="nvim" >> ~/.bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
